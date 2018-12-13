08c0c38fd6800d04c6f3a863bc63b1b974a6cb88
Brian Waganer <BrianWaganerSTL@users.noreply.github.com>
2018-12-13 17:09:50 -0600

    Update metricsCpu.py
    Fixed the len number, I was off my 1.
    Had a typo in the cpuDetail line, had an extra qoute

--- metrics/views/metricsCpu.py
+++ metrics/views/metricsCpu.py
@@ -11,12 +11,12 @@ def CpuList(request):
         a = psutil.cpu_times_percent()
 
         if platform == "linux" or platform == "linux2":
-            if len(a) == 7:
+            if len(a) == 8:
                 cpuDetails = '"user":%d,"nice":%s,"system":%d,"idle":%d,"iowait":%d,"irq":%d,"softirq":"%d,"steal":%d}' % a
-            elif len(a) == 8:
-                cpuDetails = '"user":%d,"nice":%s,"system":%d,"idle":%d,"iowait":%d,"irq":%d,"softirq":"%d,"steal":%d,"guest":%d}' % a
             elif len(a) == 9:
-                cpuDetails = '"user":%d,"nice":%s,"system":%d,"idle":%d,"iowait":%d,"irq":%d,"softirq":"%d,"steal":%d,"guest":%d,"guest_nice":%d}' % a
+                cpuDetails = '"user":%d,"nice":%s,"system":%d,"idle":%d,"iowait":%d,"irq":%d,"softirq":"%d,"steal":%d,"guest":%d}' % a
+            elif len(a) == 10:
+                cpuDetails = '"user":%d,"nice":%s,"system":%d,"idle":%d,"iowait":%d,"irq":%d,"softirq":%d,"steal":%d,"guest":%d,"guest_nice":%d}' % a
 
         elif platform == "darwin":  # OS X
             cpuDetails = '"user":%d,"nice":%s,"system":%d,"idle":%d}' % a
@@ -25,6 +25,7 @@ def CpuList(request):
             cpuDetails = '"user":%d,"system":%d,"idle":%d,"interrupt":%d,"dpc":%d}' % a
 
         myJson = '{"created_dttm":"%s", %s' % (str(timezone.now().replace(microsecond=0)), cpuDetails)
+        print('myJson=' + myJson)
         return HttpResponse(myJson)
 
 
