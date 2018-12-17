2bfc11b35cb34eae01910f672686b7ed4b37cc40
Brian Waganer <BrianWaganerSTL@users.noreply.github.com>
2018-12-16 20:10:03 -0600

    Update metricsPingDb.py

--- metrics/views/metricsPingDb.py
+++ metrics/views/metricsPingDb.py
@@ -5,39 +5,39 @@ from RocketDBaaS_minion.settings import MINION_DB, MINION_DB_USER, MINION_DB_PWD
 
 def PingDbList(request):
     if request.method == 'GET':
-       dbms_type = request.GET.get('dbms')
-       # passwd = request.GET.get('passwd')
+        dbms_type = request.GET.get('dbms')
+        # passwd = request.GET.get('passwd')
 
-       if (dbms_type == 'PostgreSQL'):
-           ping_db_status = 'Normal'
-           startTs = timezone.now()
-           try:
-               conn = psycopg2.connect(dbname=MINION_DB, user=MINION_DB_USER, password=MINION_DB_PWD, host="localhost", port=MINION_DB_PORT)
-               conn.close()
-           except psycopg2.Error as e:
-               print("Unable to connect!")
-               print(e.pgerror)
-               print(e.diag.message_primary)
-               print(e.diag.message_detail)
-               ping_db_status = 'Critical'
-           stopTs = timezone.now()
-           myJson = '{"created_dttm":"%s","ping_db_status":"%s","ping_db_response_ms":%d}' % \
+        if (dbms_type == 'PostgreSQL'):
+            ping_db_status = 'Normal'
+            startTs = timezone.now()
+            try:
+                conn = psycopg2.connect(dbname=MINION_DB, user=MINION_DB_USER, password=MINION_DB_PWD, host="localhost", port=MINION_DB_PORT)
+                conn.close()
+            except psycopg2.Error as e:
+                print("Unable to connect!")
+                print(e.pgerror)
+                print(e.diag.message_primary)
+                print(e.diag.message_detail)
+                ping_db_status = 'Critical'
+            stopTs = timezone.now()
+            myJson = '{"created_dttm":"%s","ping_db_status":"%s","ping_db_response_ms":%d}' % \
                      (str(timezone.now().replace(microsecond=0)), \
                       ping_db_status, \
                       int((stopTs - startTs).total_seconds() * 1000))
-           return HttpResponse(myJson)
+            return HttpResponse(myJson)
 
-       elif (dbms_type == 'MongoDB'):
-           ping_db_status = 'Normal'
-           startTs = timezone.now()
-           try:
-               conn = psycopg2.connect(dbname=MINION_DB, user=MINION_DB_USER, password=MINION_DB_PWD, host="localhost", port=MINION_DB_PORT)
-               conn.close()
-           except:
-               ping_db_status = 'Blackout'
-           stopTs = timezone.now()
-           myJson = '{"created_dttm":"%s","ping_db_status":"%s","ping_db_response_ms":%d}' % \
+        elif (dbms_type == 'MongoDB'):
+            ping_db_status = 'Normal'
+            startTs = timezone.now()
+            try:
+                conn = psycopg2.connect(dbname=MINION_DB, user=MINION_DB_USER, password=MINION_DB_PWD, host="localhost", port=MINION_DB_PORT)
+                conn.close()
+            except:
+                ping_db_status = 'Blackout'
+            stopTs = timezone.now()
+            myJson = '{"created_dttm":"%s","ping_db_status":"%s","ping_db_response_ms":%d}' % \
                     (str(timezone.now().replace(microsecond=0)), \
                      ping_db_status, \
                      int((stopTs - startTs).total_seconds() * 1000))
-           return HttpResponse(myJson)
+            return HttpResponse(myJson)
