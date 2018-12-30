8e48f97cc18958f45aab41e4d97de3fc4b04c78e
Brian Waganer <yoursolutions.stl@gmail.com>
2018-12-29 22:48:36 -0600

    Added a metricsHostDetails to get mimimal information for poolServers.

--- metrics/urls.py
+++ metrics/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from metrics.views import metricsCpu, metricsMountPoints, metricsLoad, metricsPingDb
+from metrics.views import metricsCpu, metricsMountPoints, metricsLoad, metricsPingDb, metricsHostDetails
 
 urlpatterns = [
     path('metrics/cpu', metricsCpu.CpuList),
@@ -8,4 +8,5 @@ urlpatterns = [
     # path('metrics/pingserver', metricsPingDb.PingDbList),  # Not needed since it's just a ping
     path('metrics/load', metricsLoad.LoadList),
     path('metrics/mountpoints', metricsMountPoints.MountPointsList),
+    path('metrics/hostdetails', metricsHostDetails.HostDetailsList),
 ]
--- metrics/views/metricsHostDetails.py
+++ metrics/views/metricsHostDetails.py
@@ -1,3 +1,5 @@
+import datetime
+from sys import platform
 import psutil
 
 # ipAddress
@@ -8,8 +10,36 @@ import psutil
 # dbVersion
 # lastServerRestart
 # lastDbRestart
+from django.http import HttpResponse
 
-def CpuList(request):
+
+def HostDetailsList(request):
     if request.method == 'GET':
-        a = psutil.cpu_count()
-        b = psutil.reboot()
+        cpuCount = psutil.cpu_count()
+
+        lastReboot = datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+
+        ramGb = round(psutil.virtual_memory().total / 1024 /1024 / 1024)
+
+        if platform == "linux" or platform == "linux2":
+            osVersion = "Linux"
+        elif platform == "darwin":
+            osVersion = "iOS"
+        elif platform == "win32":
+            osVersion = "Windows"
+
+        dbVersion = "?"
+        # if (osVersion == "Linux"):
+        #     try:
+        #         pwd.getpwnam('postgres')
+        #         dbVersion = 'PostgeSQL'
+        #     except KeyError:
+        #         try:
+        #             pwd.getpwnam('mongod')
+        #             dbVersion = 'MongoDB'
+        #         except KeyError:
+        #             dbVersion = "?"
+
+        myJson = '[{"cpuCount":%d,"lastReboot":"%s","ramGb":%d,"osVersion":"%s","dbVersion":"%s"}]' % (cpuCount, lastReboot, ramGb, osVersion, dbVersion)
+        print('myJson=' + myJson)
+        return HttpResponse(myJson)
