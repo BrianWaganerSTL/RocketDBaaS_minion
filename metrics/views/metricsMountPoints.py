5b4ed4060dd193147c98006d348574a0591eeaf3
Brian Waganer <yoursolutions.stl@gmail.com>
2019-01-15 02:18:09 -0600

    Fixed a bug with the Mountpoint getting data because of window C://, removed ://

--- metrics/views/metricsMountPoints.py
+++ metrics/views/metricsMountPoints.py
@@ -12,14 +12,16 @@ def MountPointsList(request):
         myJson = '['
         count = 0
         for p in partitions:
+            print(p.fstype + ',' + p.mountpoint)
             if (p.fstype):
                 count += 1
                 if (count > 1):
                     myJson += ','
+
                 mntpoint = psutil.disk_usage(p.mountpoint)
                 myJson += '{"created_dttm":"%s","mount_point":"%s","allocated_gb":%d,"used_gb":%d,"used_pct":%d}' % \
                          (str(timezone.now().replace(microsecond=0)), \
-                          p.mountpoint, \
+                          str(p.mountpoint).replace(':\\',''), \
                           mntpoint.total / 1024 / 1024 / 1024, \
                           mntpoint.used / 1024 / 1024 / 1024, \
                           mntpoint.percent)
